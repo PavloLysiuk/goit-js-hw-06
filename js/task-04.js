9ee8b07f6b91dd2fc24e0d5d7ccac929dafa58f5
Pavlo Lysiuk <pavel.lysiuk@gmail.com>
2023-06-14 15:26:41 +0300

    task 4 done

---
+++ js/task-04.js
@@ -0,0 +1,22 @@
+// Лічильник складається зі спану і кнопок, які по кліку повинні збільшувати і зменшувати його значення на одиницю.
+// Створи змінну counterValue, в якій буде зберігатися поточне значення лічильника та ініціалізуй її значенням 0.
+// Додай слухачів кліків до кнопок, всередині яких збільшуй або зменшуй значення лічильника.
+// Оновлюй інтерфейс новим значенням змінної counterValue.
+
+const decrementBtn = document.querySelector('button[data-action="decrement"')
+console.log(decrementBtn);
+const incrementBtn = document.querySelector('button[data-action="increment"')
+console.log(decrementBtn);
+const currentValue = document.querySelector('#value')
+
+let counterValue = 0
+
+decrementBtn.addEventListener('click', event =>{
+  counterValue -= 1;
+  currentValue.textContent = counterValue;
+});
+
+incrementBtn.addEventListener('click', event =>{
+  counterValue += 1;
+  currentValue.textContent = counterValue;
+});
