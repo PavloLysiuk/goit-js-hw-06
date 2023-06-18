1b95379096885951b819667079bda946096b48b7
Pavlo Lysiuk <pavel.lysiuk@gmail.com>
2023-06-18 20:20:00 +0300

    task 9 done

--- js/task-09.js
+++ js/task-09.js
@@ -3,13 +3,20 @@
 // Для генерування випадкового кольору використовуй функцію getRandomHexColor.
 
 const btn = document.querySelector('.change-color');
-const textColor = document.querySelector('.color');
+const text = document.querySelector('.color');
 const body = document.querySelector('body');
 
+btn.addEventListener('click', onClick);
+
+function onClick() {
+  let bodyBackground = getRandomHexColor();
+
+  body.style.backgroundColor = bodyBackground;
+  text.textContent = bodyBackground;
+}
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
-
